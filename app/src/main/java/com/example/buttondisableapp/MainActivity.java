1ec9d1512e5eab1646769c6f1a4452c881428d6a
Igor Nguyen Van <10481163u@qed.local>
2024-11-04 08:25:16 +0100

    first commit

---
+++ app/src/main/java/com/example/buttondisableapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.buttondisableapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonDisable, buttonEnable;
+    private TextView licznik;
+    private int liczbaKlikniec = 0;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonDisable = findViewById(R.id.buttonDisable);
+        buttonEnable = findViewById(R.id.buttonEnable);
+        licznik = findViewById(R.id.licznik);
+
+        buttonDisable.setOnClickListener(v -> {
+            liczbaKlikniec++;
+            buttonDisable.setEnabled(false);
+            licznik.setText("Liczba kliknięć: " + liczbaKlikniec);
+            Toast.makeText(MainActivity.this, "Duży przycisk został wyłączony", Toast.LENGTH_SHORT).show();
+        });
+
+        buttonEnable.setOnClickListener(v -> {
+            buttonDisable.setEnabled(true);
+            Toast.makeText(MainActivity.this, "Duży przycisk został aktywowany", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
